41137e363f8373e2f144626da202ac537372f7f6
maredhat <mr.mr.aaa9@gmail.com>
2023-12-09 02:00:23 +0300

    Добавлена система Кэша и обновление ядра парсера

--- parser/kernel.py
+++ parser/kernel.py
@@ -36,16 +36,30 @@ class Kernel:
                 async with session.post(self.url + f"index.php?do=search&subaction=search&search_start=&full_search=0&story={name_search}") as response:
                     html = await response.text()
                     
-                    base = BeautifulSoup(html, "html.parser")
+                    data = { "title": [], "href": [], "text-game": [] }
                     
-                    Titles = base.find_all("div", {"class", "main-news-title"})
-                     
-            
-            
+                    base = BeautifulSoup(html, "html.parser")    
+                    base_titles = base.find_all("div", {"class", "main-news-title"})
+                    base_news = base.find_all("div", {"class", "main-news-text"})
+                    
+                    for item in base_titles: 
+                        base_info = item.find("a")
+                        
+                        if base_info is not None:
+                            data["href"].append(base_info.get("href"))
+                            data["title"].append(base_info.get_text().strip())
+                            
+                    for item in base_news:
+                        data["text-game"].append(item.get_text())
+                            
+                            
+                    print(data["text-game"][-1])
+                    return data
+                                            
         except Exception as err:
             if self.debug: print(err)
             
             
-print(asyncio.run(Kernel().get_max_pages("war")))
+print(asyncio.run(Kernel().get_main_informations(1, "war")))
 
 
